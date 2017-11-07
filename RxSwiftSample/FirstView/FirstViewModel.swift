988b9da8d1896496f31ad482162528f0cd0c9509
奥村晋太郎 <flekystyley@ezweb.ne.jp>
2017-11-07 23:03:35 +0900

    FirstCommit

---
+++ RxSwiftSample/FirstView/FirstViewModel.swift
@@ -0,0 +1,16 @@
+//
+//  FirstViewModel.swift
+//  RxSwiftSample
+//
+//  Created by 奥村晋太郎 on 2017/11/07.
+//  Copyright © 2017年 ShintarouOkumura. All rights reserved.
+//
+
+import Foundation
+import RxSwift
+
+
+final class FirstViewModel {
+    fileprivate let bag = DisposeBag()
+    
+}
